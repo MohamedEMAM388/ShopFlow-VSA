0feaa16f15bd1da59a3b64602e1cc4b405d3273f
MohamedEMAM388 <mohamedemam2302301@gmail.com>
2026-08-29 18:46:32 +0300

    edit cart

--- API/Models/Product.cs
+++ API/Models/Product.cs
@@ -2,5 +2,11 @@ namespace API.Models;
 
 public class Product
 {
-    
+    public string Id { get; set; } = null!;
+
+    public string Name { get; set; } = null!;
+
+    public string Description { get; set; } = null!;
+
+    public decimal Price { get; set; }
 }
